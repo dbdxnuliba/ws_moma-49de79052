a51d57d2cdd5d4f6d8b66033e9f6b7194accc540
guangxiao.yang <guangxiao.yang@tmirob.com>
2022-05-20 22:14:44 +0800

    first commit

---
+++ src/moma-49de7905265528681bf4712a8415194b6fee1c45/install_dependencies.sh
@@ -0,0 +1,22 @@
+sudo apt-get install \
+	ros-$ROS_DISTRO-ros-control \
+	ros-$ROS_DISTRO-ros-controllers \
+	ros-$ROS_DISTRO-gazebo-ros-pkgs \
+	ros-$ROS_DISTRO-gazebo-ros-control \
+	ros-$ROS_DISTRO-gazebo-ros \
+	ros-$ROS_DISTRO-ridgeback-desktop \
+	ros-$ROS_DISTRO-ridgeback-simulator \
+	ros-$ROS_DISTRO-moveit \
+	ros-$ROS_DISTRO-rosmon \
+	ros-$ROS_DISTRO-pcl-ros \
+	ros-$ROS_DISTRO-tf2-sensor-msgs \
+	ros-$ROS_DISTRO-py-trees \
+	ros-$ROS_DISTRO-py-trees-ros \
+	ros-$ROS_DISTRO-rqt-py-trees \
+	ros-$ROS_DISTRO-libfranka \
+	ros-$ROS_DISTRO-joint-state-publisher-gui \
+	ros-$ROS_DISTRO-ddynamic-reconfigure \
+	ros-$ROS_DISTRO-lms1xx \
+	ros-$ROS_DISTRO-interactive-marker-twist-server \
+	ros-$ROS_DISTRO-ros-numpy \
+	qtbase5-dev
